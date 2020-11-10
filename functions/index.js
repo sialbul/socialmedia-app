b0770f91bc99000f5ccf4e960ad91053b9d7231c
Sibel <sbaslamisli@yahoo.com>
2020-11-09 21:18:08 -0500

    still have firebase deploy problem

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,5 @@
+const { admin } = require('firebase-admin/lib/credential');
 const functions = require('firebase-functions');
-// The Firebase Admin SDK to access Cloud Firestore.
-const admin = require('firebase-admin');
-admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -11,13 +9,35 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
 });
 
-// Take the text parameter passed to this HTTP endpoint and insert it into 
-// Cloud Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.https.onRequest(async(req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into Cloud Firestore using the Firebase Admin SDK.
-    const writeResult = await admin.firestore().collection('messages').add({ original: original });
-    // Send back a message that we've succesfully written the message
-    res.json({ result: `Message with ID: ${writeResult.id} added.` });
-});
+// exports.getScreams = functions.https.onRequest((req, res) => {
+//     admin
+//         .firestore()
+//         .collection('screams')
+//         .get()
+//         .then((data) => {
+//             let screams = [];
+//             data.forEach((doc) => {
+//                 screams.push(doc.data());
+//             });
+//             return res.json(screams);
+//         })
+//         .catch((err) => console.error(err));
+// });
+
+// exports.createScream = functions.https.onRequest((req, res) => {
+//     const newScream = {
+//         body: req.body.body,
+//         userHandle: req.body.userHandle,
+//         createdAt: admin.firestore.Timestamp.fromDate(new Date())
+//     };
+//     admin.firestore()
+//         .collection('screams')
+//         .add(newScream)
+//         .then(doc => {
+//             res.json({ message: `document ${doc.id} created successfully` });
+//         })
+//         .catch(err => {
+//             res.status(500).json({ error: 'something went wrong' });
+//             console.error(err);
+//         })
+// })
