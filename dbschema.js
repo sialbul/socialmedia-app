1d0c278ded22dabb1712050bd6363bcbfda024a7
Sibel <sbaslamisli@yahoo.com>
2020-11-10 21:49:48 -0500

    user registration setup

---
+++ dbschema.js
@@ -0,0 +1,12 @@
+let db = {
+
+    screams: [{
+
+        userHandle: 'user',
+        body: 'this is the scream body',
+        createdAt: '2020-11-11T02:17:15.367Z',
+        likeCount: 5,
+        commentCount: 2,
+    }]
+
+}
--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,37 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
+const app = require('express')();
 
 admin.initializeApp();
-const express = require('express');
-const app = express();
+
+const config = {
+    apiKey: "AIzaSyBCV1Y9TtrFjo7CP51nmim6iLlcZuoDJiE",
+    authDomain: "socialapp-d7997.firebaseapp.com",
+    databaseURL: "https://socialapp-d7997.firebaseio.com",
+    projectId: "socialapp-d7997",
+    storageBucket: "socialapp-d7997.appspot.com",
+    messagingSenderId: "504905807989",
+    appId: "1:504905807989:web:d526aed5098755c2c2a03c",
+    measurementId: "G-G4KT0EXKMZ"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
 
 app.get('/screams', (req, res) => {
-    admin.firestore().collection('screams').get()
+    admin.firestore()
+        .collection('screams')
+        .orderBy('createdAt', 'desc')
+        .get()
         .then((data) => {
             let screams = [];
             data.forEach((doc) => {
-                screams.push(doc.data());
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                });
             });
             return res.json(screams);
         })
@@ -18,13 +39,10 @@ app.get('/screams', (req, res) => {
 });
 
 app.post('/scream', (req, res) => {
-    if (req.method !== 'POST') {
-        return res.status(400).json({ error: 'Method not allowed' });
-    }
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
     admin.firestore()
         .collection('screams')
@@ -38,6 +56,28 @@ app.post('/scream', (req, res) => {
         })
 })
 
-// https://baseurl.com/api/
+//signup route
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    };
+
+    //TODO validate data
+
+    firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then(data => {
+            return res.status(201).json({ message: `user ${data.user.uid} signed up successfully` })
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err, code });
+        });
+})
+
+
+
 
 exports.api = functions.https.onRequest(app);
