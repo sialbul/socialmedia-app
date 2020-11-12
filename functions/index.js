9018cdfe6c24a5440a9f8608e45a71d08fe55963
Sibel <sbaslamisli@yahoo.com>
2020-11-12 13:15:53 -0500

    validation & login route

--- functions/index.js
+++ functions/index.js
@@ -56,6 +56,18 @@ app.post('/scream', (req, res) => {
         })
 })
 
+//validation
+
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.match(regEx)) return true;
+    else return false;
+}
+const isEmpty = (string) => {
+    if (string === '') return true;
+    else return false;
+}
+
 //signup route
 app.post('/signup', (req, res) => {
     const newUser = {
@@ -65,7 +77,21 @@ app.post('/signup', (req, res) => {
         handle: req.body.handle,
     };
 
-    //TODO validate data
+    let errors = {};
+
+    if (isEmpty(newUser.email)) {
+        errors.email = 'Must not be empty'
+    } else if (!isEmail(newUser.email)) {
+        errors.email = 'Must be valid email adress'
+    }
+
+    if (isEmpty(newUser.password)) errors.password = 'Must not be empty'
+    if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match'
+    if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty'
+
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors)
+
+    //validate data
     let token, userId;
 
     db.doc(`/users/${newUser.handle}`).get()
