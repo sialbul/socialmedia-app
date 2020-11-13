6c6e20e4fd26932606c47648517c03b91b22187d
Sibel <sbaslamisli@yahoo.com>
2020-11-13 13:50:46 -0500

    refactoring&organising codes

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyBCV1Y9TtrFjo7CP51nmim6iLlcZuoDJiE",
+    authDomain: "socialapp-d7997.firebaseapp.com",
+    databaseURL: "https://socialapp-d7997.firebaseio.com",
+    projectId: "socialapp-d7997",
+    storageBucket: "socialapp-d7997.appspot.com",
+    messagingSenderId: "504905807989",
+    appId: "1:504905807989:web:d526aed5098755c2c2a03c",
+    measurementId: "G-G4KT0EXKMZ"
+};
