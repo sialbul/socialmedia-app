69069cabfce1418f92a03ce5b7ca8733cf7c6947
Sibel <sbaslamisli@yahoo.com>
2020-11-19 13:49:29 -0500

    comment on post

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,14 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/fbauth')
-const { getAllScreams, postOneScream, getScream } = require('./handlers/screams');
+const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams');
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
 
 //scream routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
 app.get('/scream/:screamId', getScream);
-
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 
 //user routes
@@ -16,7 +16,8 @@ app.post('/signup', signup);
 app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
-app.get('/user', FBAuth, getAuthenticatedUser)
+app.get('/user', FBAuth, getAuthenticatedUser);
+
 
 
 exports.api = functions.https.onRequest(app);
