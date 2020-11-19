ae5498e166fbb2572278f2d2421a70aadc9a698f
Sibel <sbaslamisli@yahoo.com>
2020-11-18 21:26:02 -0500

    getting user data

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -93,17 +93,41 @@ exports.login = (req, res) => {
 //Add user details
 exports.addUserDetails = (req, res) => {
     let userDetails = reduceUserDetails(req.body);
-    db.doc(`/users.${req.user.handle}`).update(userDetails).then(() => {
-            return res.json({ message: 'Details added successfully' });
+
+    db.doc(`/users/${req.user.handle}`)
+        .update(userDetails)
+        .then(() => {
+            return res.json({ message: "Details added successfully" });
         })
-        .catch(err => {
+        .catch((err) => {
             console.error(err);
             return res.status(500).json({ error: err.code });
-        })
+        });
+};
 
-}
-
-//upload a profile photo for user
+//get own user details
+exports.getAuthenticatedUser = (req, res) => {
+        let userData = {};
+        db.doc(`/users/${req.user.handle}`).get()
+            .then((doc) => {
+                if (doc.exists) {
+                    userData.credentials = doc.data();
+                    return db.collection('likes').where("userHandle", '==', req.user.handle).get();
+                }
+            })
+            .then((data) => {
+                userData.likes = [];
+                data.forEach(doc => {
+                    userData.likes.push(doc.data());
+                });
+                return res.json(userData);
+            })
+            .catch((err) => {
+                console.error(err);
+                return res.status(500).json({ error: err.code });
+            });
+    }
+    //upload a profile photo for user
 exports.uploadImage = (req, res) => {
     const Busboy = require('busboy');
     const path = require('path');
