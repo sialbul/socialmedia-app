9447ae2aa5e137f310cbd4dfaac524a66fd31282
Sibel <sbaslamisli@yahoo.com>
2020-11-20 00:31:13 -0500

    delete a post

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -96,6 +96,9 @@ exports.commentOnScream = (req, res) => {
             if (!doc.exists) {
                 return res.status(404).json({ error: 'Scream not found' });
             }
+            return doc.ref.update({ commentCount: doc.data().commentCount + 1 });
+        })
+        .then(() => {
             return db.collection('comments').add(newComment);
         })
         .then(() => {
@@ -182,5 +185,27 @@ exports.unlikeScream = (req, res) => {
         })
         .catch(err => {
             res.status(500).json({ error: err.code });
+        })
+};
+
+exports.deleteScream = (req, res) => {
+    const document = db.doc(`/screams/${req.params.screamId}`);
+    document.get()
+        .then(doc => {
+            if (!doc.exists) {
+                return res.status(404).json({ error: 'Scream not found' });
+            }
+            if (doc.data().userHandle !== req.user.handle) {
+                return res.status(403).json({ error: 'Unauthorized' });
+            } else {
+                return document.delete();
+            }
+        })
+        .then(() => {
+            res.json({ message: 'Scream deleted successfully' });
+
+        })
+        .catch(err => {
+            return res.status(500).json({ error: err.code });
         })
 }
--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,14 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/fbauth')
-const { getAllScreams, postOneScream, getScream, commentOnScream, likeScream, unlikeScream } = require('./handlers/screams');
+const { getAllScreams, postOneScream, getScream, commentOnScream, likeScream, unlikeScream, deleteScream } = require('./handlers/screams');
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
 
 //scream routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
 app.get('/scream/:screamId', getScream);
+app.delete('/scream/:screamId', FBAuth, deleteScream);
 
 app.get('/scream/:screamId/like', FBAuth, likeScream);
 app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
