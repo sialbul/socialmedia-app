25e20f119121f918d9a13baa566e1e18f565f430
Sibel <sbaslamisli@yahoo.com>
2020-11-20 00:44:32 -0500

    notifications

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,8 @@ const FBAuth = require('./util/fbauth')
 const { getAllScreams, postOneScream, getScream, commentOnScream, likeScream, unlikeScream, deleteScream } = require('./handlers/screams');
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
 
+const { db } = require('./util/admin');
+
 //scream routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
@@ -25,3 +27,27 @@ app.get('/user', FBAuth, getAuthenticatedUser);
 
 
 exports.api = functions.https.onRequest(app);
+
+exports.createNotificationOnLike = functions.firestore.document('likes/{id')
+    .onCreate((snapshot) => {
+        db.doc(`/screams/${snapshot.data().screamId}`).get()
+            .then(doc => {
+                if (doc.exists) {
+                    return db.doc(`/notifications/${snapshot.id}`).set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: 'comment',
+                        read: false,
+                        screamId: doc.id
+                    })
+                }
+            })
+            .then(() => {
+                return;
+            })
+            .catch((err) => {
+                console.error(err);
+                return;
+            });
+    })
