511f1a46dc2269f2da60f2cb478d7e0777a1c80e
Sibel <sbaslamisli@yahoo.com>
2020-11-19 20:36:45 -0500

    like a post

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -147,5 +147,40 @@ exports.likeScream = (req, res) => {
 
 }
 exports.unlikeScream = (req, res) => {
+    const likeDocument = db
+        .collection('likes')
+        .where('userHandle', '==', req.user.handle)
+        .where('screamId', '==', req.params.screamId)
+        .limit(1);
+    const screamDocument = db.doc(`/screams/${req.params.screamId}`);
 
+    let screamData;
+    screamDocument.get()
+        .then(doc => {
+            if (doc.exists) {
+                screamData = doc.data();
+                screamData.screamId = doc.id;
+                return likeDocument.get();
+            } else {
+                return res.status(404).json({ errors: 'Scream not found' });
+            }
+        })
+        .then(data => {
+            if (data.empty) {
+                return res.status(400).json({ error: 'Scream not liked' });
+
+            } else {
+                return db.doc(`/likes/${data.docs[0].id}`).delete()
+                    .then(() => {
+                        screamData.likeCount--;
+                        return screamDocument.update({ likeCount: screamData.likeCount });
+                    })
+                    .then(() => {
+                        res.json(screamData);
+                    })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: err.code });
+        })
 }
