851ebe232cd2d65bb5200a42210b8734cf791714
Sibel <sbaslamisli@yahoo.com>
2020-11-22 00:19:09 -0500

    notifications

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -5,6 +5,7 @@ const firebase = require('firebase');
 firebase.initializeApp(config)
 
 const { validateSignupData, validateLoginData, reduceUserDetails } = require('../util/validators');
+const { user } = require('firebase-functions/lib/providers/auth');
 
 //Sign Users
 exports.signup = (req, res) => {
@@ -110,12 +111,42 @@ exports.getUserDetails = (req, res) => {
     let userData = {};
     db.doc(`/users/${req.params.handle}`).get()
         .then(doc => {
+            if (doc.exists) {
+                userData.user = doc.data();
+                return db.collection('screams').where('userHandle', '==', req.params.handle)
+                    .orderBy('createdAt', 'desc')
+                    .get();
+            } else {
+                return res.status(404).json({ error: "User not found" })
+            }
 
-
+        })
+        .then(data => {
+            userData.screams = [];
+            data.forEach(doc => {
+                userData.screams.push({
+                    body: doc.data().body,
+                    createdAt: doc.data().createdAt,
+                    userHandle: doc.data().userHandle,
+                    userImage: doc.data().userImage,
+                    likeCount: doc.data().likeCount,
+                    commentCount: doc.data().commentCount,
+                    screamId: doc.id
+                })
+            });
+            return res.json(userData);
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
         })
 }
 
-//get own user details
+
+exports.markNotificationsRead = (req, res) => {
+
+    }
+    //get own user details
 exports.getAuthenticatedUser = (req, res) => {
     let userData = {};
     db.doc(`/users/${req.user.handle}`).get()
