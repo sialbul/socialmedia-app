60312546923c5b2f1edd481834b7b2d9d11acccd
Sibel <sbaslamisli@yahoo.com>
2020-11-24 00:37:26 -0500

    new triggers - cancel notifications when scream delete

--- functions/index.js
+++ functions/index.js
@@ -108,8 +108,8 @@ exports.onUserImageChange = functions.firestore.document('/users/{userId}')
                         batch.update(scream, { userImage: change.after.data().imageUrl })
                     })
                     return batch.commit();
-                })
-        }
+                });
+        } else return true;
     });
 
 exports.onScreamDelete = functions.firestore.document('/users/{userId}')
@@ -121,13 +121,13 @@ exports.onScreamDelete = functions.firestore.document('/users/{userId}')
                 data.forEach(doc => {
                     batch.delete(db.doc(`/comments/${doc.id}`));
                 })
-                return db.collection('likes').where('screamId', '==', screamId);
+                return db.collection('likes').where('screamId', '==', screamId).get();
             })
             .then(data => {
                 data.forEach(doc => {
                     batch.delete(db.doc(`/likes/${doc.id}`));
                 })
-                return db.collection('notifications').where('screamId', '==', screamId);
+                return db.collection('notifications').where('screamId', '==', screamId).get();
             })
             .then(data => {
                 data.forEach(doc => {
