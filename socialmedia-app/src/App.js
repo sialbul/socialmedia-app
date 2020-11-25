bf2960234d282e56ef8b542373afacbcf981746a
Sibel <sbaslamisli@yahoo.com>
2020-11-24 23:54:56 -0500

    navbar

--- socialmedia-app/src/App.js
+++ socialmedia-app/src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
+import {BrowserRouter as Router,Route, Switch } from 'react-router-dom';
+
+//Components
+import Navbar from './components/Navbar';
+
+//Pages
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+
 
 function App() {
     return (
         <div className="App">
-            <h1>Our app</h1>
+            <Router>
+              <Navbar />
+              <Switch>
+                <Route path="/" exact component={home}/>
+                <Route path="/login" component={login}/>
+                <Route path="/signup" component={signup}/>
+              </Switch>
+            </Router>
         </div>
     );
 }
--- socialmedia-app/src/components/Navbar.js
+++ socialmedia-app/src/components/Navbar.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
-import "../App.css";
+
+//MUI stuff
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Toolbar";
 
 export class Navbar extends Component {
     render() {
-        return <div></div>;
+        return (
+            <div>
+                <AppBar>
+                    <Toolbar>
+                        <Button color="inherit"> Login</Button>
+                        <Button color="inherit"> Home</Button>
+                        <Button color="inherit"> Signup</Button>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
     }
 }
 
