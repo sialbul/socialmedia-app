ad74cd9777f163619bc719bfc8240c091b7d12e1
Sibel <sbaslamisli@yahoo.com>
2020-11-24 22:58:11 -0500

    react setup

---
+++ socialmedia-app/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../App.css";
+
+export class Navbar extends Component {
+    render() {
+        return <div></div>;
+    }
+}
+
+export default Navbar;
