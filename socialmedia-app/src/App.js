a9ede002e2d9e8435a395897049dc611f8e0f052
Sibel <sbaslamisli@yahoo.com>
2020-11-25 00:37:47 -0500

    navbar

--- socialmedia-app/src/App.js
+++ socialmedia-app/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import {BrowserRouter as Router,Route, Switch } from 'react-router-dom';
-
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 //Components
 import Navbar from './components/Navbar';
 
@@ -9,10 +10,30 @@ import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
+const theme =createMuiTheme({
+  
+    palette: {
+      primary: {
+        light: '#d7e360',
+        main:'#cddc39',
+        dark:'#8f9a27',
+        contrastText: '#fff'
+      }
+      ,
+      secondary: {
+      light: '#f73378',
+      main:'#f50057',
+      dark:'#ab003c',
+      contrastText: '#fff'
+    }},
+    })
+  
+
 
 function App() {
     return (
-        <div className="App">
+        <MuiThemeProvider theme={theme}>
+          <div className="App">
             <Router>
             <Navbar />
             <div className="container">
@@ -25,6 +46,7 @@ function App() {
             </div>
             </Router>
         </div>
+        </MuiThemeProvider>
     );
 }
 
