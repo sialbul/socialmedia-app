3c9f375ffb3bad76b953a9a347bbaf60fd059406
Sibel <sbaslamisli@yahoo.com>
2020-11-25 00:50:45 -0500

    navbar

--- socialmedia-app/src/App.js
+++ socialmedia-app/src/App.js
@@ -1,51 +1,47 @@
 import "./App.css";
-import {BrowserRouter as Router,Route, Switch } from 'react-router-dom';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 //Components
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 //Pages
-import home from './pages/home';
-import login from './pages/login';
-import signup from './pages/signup';
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
 
-const theme =createMuiTheme({
-  
+const theme = createMuiTheme({
     palette: {
-      primary: {
-        light: '#d7e360',
-        main:'#cddc39',
-        dark:'#8f9a27',
-        contrastText: '#fff'
-      }
-      ,
-      secondary: {
-      light: '#f73378',
-      main:'#f50057',
-      dark:'#ab003c',
-      contrastText: '#fff'
-    }},
-    })
-  
-
+        primary: {
+            light: "#d7e360",
+            main: "#cddc39",
+            dark: "#8f9a27",
+            contrastText: "#fff",
+        },
+        secondary: {
+            light: "#f73378",
+            main: "#f50057",
+            dark: "#ab003c",
+            contrastText: "#fff",
+        },
+    },
+});
 
 function App() {
     return (
         <MuiThemeProvider theme={theme}>
-          <div className="App">
-            <Router>
-            <Navbar />
-            <div className="container">
-            <Switch>
-                <Route path="/" exact component={home}/>
-                <Route path="/login" component={login}/>
-                <Route path="/signup" component={signup}/>
-              </Switch>
-
+            <div className="App">
+                <Router>
+                    <Navbar />
+                    <div className="container">
+                        <Switch>
+                            <Route path="/" exact component={home} />
+                            <Route path="/login" component={login} />
+                            <Route path="/signup" component={signup} />
+                        </Switch>
+                    </div>
+                </Router>
             </div>
-            </Router>
-        </div>
         </MuiThemeProvider>
     );
 }
--- socialmedia-app/src/pages/home.js
+++ socialmedia-app/src/pages/home.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
 
 export class home extends Component {
+    componentDidMount(){
+        
+    }
     render() {
         return (
-            <div>
-                <h1>Home page</h1>
-            </div>
+        <Grid container spacing={16}>
+            <Grid item sm={8} xs={12}>
+                <p>Content</p>
+            </Grid>
+            <Grid item sm={4} xs={12}>
+                <p>Profile</p>
+            </Grid>
+        </Grid>
         )
     }
 }
