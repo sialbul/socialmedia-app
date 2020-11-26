ce32b13e132584fc5eae2f08a8c1369731aeabff
Sibel <sbaslamisli@yahoo.com>
2020-11-26 00:10:10 -0500

    axios is working now

--- socialmedia-app/src/components/Scream.js
+++ socialmedia-app/src/components/Scream.js
@@ -3,6 +3,7 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
+import { Typography } from "@material-ui/core";
 
 const styles = {
     card: {
@@ -12,10 +13,29 @@ const styles = {
 
 class Scream extends Component {
     render() {
-        const { classes } = this.props;
+        const {
+            classes,
+            scream: {
+                body,
+                createdAt,
+                userImage,
+                userHandle,
+                screamId,
+                likeCount,
+                commentCount,
+            },
+        } = this.props;
         return (
             <Card>
-                <CardMedia image={userImage} />
+                <CardMedia 
+                image={userImage}
+                title="Profile Image"
+                 />
+                 <CardContent>
+                     <Typography variant="h5">{userHandle}</Typography>
+                     <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+                     <Typography variant="body1">{body}</Typography>
+                 </CardContent>
             </Card>
         );
     }
