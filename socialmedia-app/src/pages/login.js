82d00b0b08177259cd2b64c669a99bc171c85f81
Sibel <sbaslamisli@yahoo.com>
2020-11-28 01:10:23 -0500

    login

--- socialmedia-app/src/pages/login.js
+++ socialmedia-app/src/pages/login.js
@@ -1,43 +1,86 @@
 import React, { Component } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
 import PropTypes from "prop-types";
-import AppIcon from '../images/head.png';
+import AppIcon from "../images/head.png";
 
 import Grid from "@material-ui/core/Grid";
 import { Typography } from "@material-ui/core";
-import TextField from '@material-ui/core/TextField';
-
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 const styles = {
     form: {
         textAlign: "center",
     },
-   
-    image:{
-        maxHeight:80,
-        margin:'20px auto 20px auto'
+
+    image: {
+        maxHeight: 80,
+        margin: "20px auto 20px auto",
+    },
+    pageTitle:{
+        margin: "10px auto 10px auto",
+    },
+    button:{
+        marginTop: 20,
     }
 };
 
-handleSubmit = (event) =>{
-    console.log('hi');
-}
-
 class login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: "",
+            password: "",
+            loading: false,
+            errors: {},
+        };
+    }
+    handleSubmit = (event) => {
+        console.log("hi");
+    };
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    };
     render() {
         const { classes } = this.props;
         return (
             <Grid container className={classes.form}>
                 <Grid item sm />
                 <Grid item sm>
-                    <img src={AppIcon} alt="Man Image" className={classes.image}/>
+                    <img
+                        src={AppIcon}
+                        alt="Man Image"
+                        className={classes.image}
+                    />
                     <Typography variant="h2" className={classes.pageTitle}>
                         Login
                     </Typography>
                     <form noValidate onSubmit={this.handleSubmit}>
-
+                        <TextField
+                            id="email"
+                            name="email"
+                            type="email"
+                            label="Email"
+                            className={classes.textField}
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                            fullWidth
+                        />
+                        <TextField
+                            id="password"
+                            name="password"
+                            type="password"
+                            label="Password"
+                            className={classes.textField}
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                            fullWidth
+                        />
+                        <Button type="submit" variant="contained" color="primary" className= {classes.button}>Submit</Button>
                     </form>
-
                 </Grid>
                 <Grid item sm />
             </Grid>
