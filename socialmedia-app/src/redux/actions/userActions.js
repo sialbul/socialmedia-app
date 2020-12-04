a03fb2f4c0c2b0add63bb29e9394ae8eaba0b739
Sibel <sbaslamisli@yahoo.com>
2020-12-03 22:14:04 -0500

    redux setup

--- socialmedia-app/src/redux/actions/userActions.js
+++ socialmedia-app/src/redux/actions/userActions.js
@@ -1,23 +1,34 @@
 import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from '../types';
 
 exports
-const loginUser = (userData) => (dispatch) => {
+const loginUser = (userData, history) => (dispatch) => {
     dispatch({ type: LOADING_UI });
+
     axios.post('/login', userData)
         .then(res => {
-            console.log(res.data);
-            localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
-            this.setState({
-                loading: false
-            });
-            this.props.history.push('/');
+            const FBIdToken = `Bearer ${res.data.token}`;
+            localStorage.setItem('FBIdToken', FBIdToken);
+            axios.defaults.headers.common['Authorization'] = FBIdToken;
+            dispatch(getUserData());
+            dispatch({ type: CLEAR_ERRORS });
+            history.push('/');
         })
         .catch(err => {
-            this.setState({
-                errors: err.response.data,
-                loading: false
-
+            dispatch({
+                type: SET_ERRORS,
+                payload: err.response.data
             })
-
         });
+}
+
+export const getUserData = () => (dispatch) => {
+    axios.get('/user')
+        .then(res => {
+            dispatch({
+                type: SET_USER,
+                payload: res
+            })
+        })
+        .catch(err => console.log(err));
+
 }
