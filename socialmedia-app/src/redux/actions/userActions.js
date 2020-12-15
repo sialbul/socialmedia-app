f0cd54dae3a463d77e7101b9c78878371168159b
Sibel <sbaslamisli@yahoo.com>
2020-12-14 22:29:56 -0500

    update

--- socialmedia-app/src/redux/actions/userActions.js
+++ socialmedia-app/src/redux/actions/userActions.js
@@ -1,34 +1,41 @@
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from '../types';
+import {
+    SET_USER,
+    SET_ERRORS,
+    CLEAR_ERRORS,
+    LOADING_UI,
+    SET_UNAUTHENTICATED,
+    LOADING_USER,
+    MARK_NOTIFICATIONS_READ
+} from '../types';
 import axios from 'axios';
 
 export const loginUser = (userData, history) => (dispatch) => {
     dispatch({ type: LOADING_UI });
-
-    axios.post('/login', userData)
+    axios
+        .post('/login', userData)
         .then((res) => {
-            const FBIdToken = `Bearer ${res.data.token}`;
-            localStorage.setItem('FBIdToken', FBIdToken);
-            axios.defaults.headers.common['Authorization'] = FBIdToken;
+            setAuthorizationHeader(res.data.token);
             dispatch(getUserData());
             dispatch({ type: CLEAR_ERRORS });
             history.push('/');
         })
-        .catch(err => {
+        .catch((err) => {
             dispatch({
                 type: SET_ERRORS,
                 payload: err.response.data
-            })
+            });
         });
-}
+};
 
 export const getUserData = () => (dispatch) => {
-    axios.get('/user')
-        .then(res => {
+    dispatch({ type: LOADING_USER });
+    axios
+        .get('/user')
+        .then((res) => {
             dispatch({
                 type: SET_USER,
                 payload: res.data
-            })
+            });
         })
-        .catch(err => console.log(err));
-
-}
+        .catch((err) => console.log(err));
+};
--- socialmedia-app/src/redux/reducers/userReducer.js
+++ socialmedia-app/src/redux/reducers/userReducer.js
@@ -1,7 +1,16 @@
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_UNAUTHENTICATED, SET_AUTHENTICATED } from '../types';
+import {
+    SET_USER,
+    SET_AUTHENTICATED,
+    SET_UNAUTHENTICATED,
+    LOADING_USER,
+    LIKE_SCREAM,
+    UNLIKE_SCREAM,
+    MARK_NOTIFICATIONS_READ
+} from '../types';
 
 const initialState = {
     authenticated: false,
+    loading: false,
     credentials: {},
     likes: [],
     notifications: []
@@ -19,8 +28,37 @@ export default function(state = initialState, action) {
         case SET_USER:
             return {
                 authenticated: true,
+                loading: false,
                 ...action.payload
-            }
+            };
+        case LOADING_USER:
+            return {
+                ...state,
+                loading: true
+            };
+        case LIKE_SCREAM:
+            return {
+                ...state,
+                likes: [
+                    ...state.likes,
+                    {
+                        userHandle: state.credentials.handle,
+                        screamId: action.payload.screamId
+                    }
+                ]
+            };
+        case UNLIKE_SCREAM:
+            return {
+                ...state,
+                likes: state.likes.filter(
+                    (like) => like.screamId !== action.payload.screamId
+                )
+            };
+        case MARK_NOTIFICATIONS_READ:
+            state.notifications.forEach((not) => (not.read = true));
+            return {
+                ...state
+            };
         default:
             return state;
     }
