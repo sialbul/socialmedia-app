1e1de8fbfc8e8a2af7c3e844bca7159f8a391d42
Sibel <sbaslamisli@yahoo.com>
2020-12-22 12:30:45 -0500

    profile image change option

--- socialmedia-app/src/util/theme.js
+++ socialmedia-app/src/util/theme.js
@@ -19,10 +19,8 @@ export default {
     form: {
         textAlign: 'center'
     },
-
     image: {
         margin: '20px auto 20px auto'
-
     },
     pageTitle: {
         margin: '10px auto 10px auto'
@@ -53,8 +51,47 @@ export default {
     },
     paper: {
         padding: 20
+    },
+    profile: {
+        '& .image-wrapper': {
+            textAlign: 'center',
+            position: 'relative',
+            '& button': {
+                position: 'absolute',
+                top: '80%',
+                left: '70%'
+            }
+        },
+        '& .profile-image': {
+            width: 200,
+            height: 200,
+            objectFit: 'cover',
+            maxWidth: '100%',
+            borderRadius: '50%'
+        },
+        '& .profile-details': {
+            textAlign: 'center',
+            '& span, svg': {
+                verticalAlign: 'middle'
+            },
+            '& a': {
+                color: '#00bcd4'
+            }
+        },
+        '& hr': {
+            border: 'none',
+            margin: '0 0 10px 0'
+        },
+        '& svg.button': {
+            '&:hover': {
+                cursor: 'pointer'
+            }
+        }
+    },
+    buttons: {
+        textAlign: 'center',
+        '& a': {
+            margin: '20px 10px'
+        }
     }
-
-
-
 };
