9482c65904426a3d39f1b3d1eabf333d3588ce02
Sibel <sbaslamisli@yahoo.com>
2020-12-23 12:59:04 -0500

    profile image change option

--- socialmedia-app/src/components/Profile.js
+++ socialmedia-app/src/components/Profile.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import {Link} from 'react-router-dom';
+import dayjs from 'dayjs';
 
 //MUI Stuff
 import Button from '@material-ui/core/Button';
@@ -10,6 +11,8 @@ import MuiLink from '@material-ui/core/Link';
 
 //Icons
 import LocationOn from '@material-ui/icons/LocationOn';
+import LinkIcon from '@material-ui/icons/Link';
+import CalendarToday from '@material-ui/icons/CalendarToday';
 
 //Redux
 import Typography from '@material-ui/core/Typography';
@@ -39,14 +42,40 @@ const styles = (theme) => ({
                         {bio && <Typography variant='body2'>{bio}</Typography> }
 <hr/>
 {location &&(
-    <LocationOn></LocationOn>
+  <Fragment>
+        <LocationOn color="primary"/> <span>{location}</span>
+    <hr/>
+  </Fragment>
 )}
+{website &&(
+    <Fragment>
+        <LinkIcon color="primary" />
+<a href={website} target="_blank" rel="noopener noreferrer">
+    {' '}{website}
+</a>
+<hr/>
+    </Fragment>
+)}
+<CalendarToday color="primary"/>{' '}
+<span>Joined {dayjs(createdAt).format('MMM YYYY')} </span>
 
                     </div>
 
                 </div>
             </Paper>
-        ):()):(<p>Loading...</p>)
+        ):( 
+<Paper className={classes.paper}>
+    <Typography variant="body2" align="center">
+        No profile found, please login again
+    </Typography>
+    <div className={classes.buttons}>
+        <Button variant="contained" color="primary" component={Link} to="/login">Login</Button>
+        <Button variant="contained" color="primary" component={Link} to="/signup">Signup</Button>
+    </div>
+</Paper>
+
+
+        )):(<p>Loading...</p>)
         return profileMarkup;
 
     }
