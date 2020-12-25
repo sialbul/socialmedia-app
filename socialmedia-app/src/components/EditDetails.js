4610e9d2c1f5ce97a1d970fa5741ac7c8f166ecc
Sibel <sbaslamisli@yahoo.com>
2020-12-25 01:04:19 -0500

    edit profile option

--- socialmedia-app/src/components/EditDetails.js
+++ socialmedia-app/src/components/EditDetails.js
@@ -1,39 +1,39 @@
-import React, { Component, Fragment } from 'react';
-import PropTypes from 'prop-types';
-import withStyles from '@material-ui/core/styles/withStyles';
-import MyButton from '../util/MyButton';
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+import MyButton from "../util/MyButton";
 // Redux stuff
-import { connect } from 'react-redux';
-import { editUserDetails } from '../redux/actions/userActions';
+import { connect } from "react-redux";
+import { editUserDetails } from "../redux/actions/userActions";
 // MUI Stuff
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
 // Icons
-import EditIcon from '@material-ui/icons/Edit';
+import EditIcon from "@material-ui/icons/Edit";
 
 const styles = (theme) => ({
     ...theme,
     button: {
-        float: 'right'
-    }
+        float: "right",
+    },
 });
 
 class EditDetails extends Component {
     state = {
-        bio: '',
-        website: '',
-        location: '',
-        open: false
+        bio: "",
+        website: "",
+        location: "",
+        open: false,
     };
     mapUserDetailsToState = (credentials) => {
         this.setState({
-            bio: credentials.bio ? credentials.bio : '',
-            website: credentials.website ? credentials.website : '',
-            location: credentials.location ? credentials.location : ''
+            bio: credentials.bio ? credentials.bio : "",
+            website: credentials.website ? credentials.website : "",
+            location: credentials.location ? credentials.location : "",
         });
     };
     handleOpen = () => {
@@ -50,85 +50,80 @@ class EditDetails extends Component {
 
     handleChange = (event) => {
         this.setState({
-            [event.target.name]: event.target.value
+            [event.target.name]: event.target.value,
         });
     };
     handleSubmit = () => {
         const userDetails = {
             bio: this.state.bio,
             website: this.state.website,
-            location: this.state.location
+            location: this.state.location,
         };
         this.props.editUserDetails(userDetails);
         this.handleClose();
     };
     render() {
         const { classes } = this.props;
-        return ( <
-            Fragment >
-            <
-            MyButton tip = "Edit Details"
-            onClick = { this.handleOpen }
-            btnClassName = { classes.button } >
-            <
-            EditIcon color = "primary" / >
-            <
-            /MyButton> <
-            Dialog open = { this.state.open }
-            onClose = { this.handleClose }
-            fullWidth maxWidth = "sm" >
-            <
-            DialogTitle > Edit your details < /DialogTitle> <
-            DialogContent >
-            <
-            form >
-            <
-            TextField name = "bio"
-            tpye = "text"
-            label = "Bio"
-            multiline rows = "3"
-            placeholder = "A short bio about yourself"
-            className = { classes.textField }
-            value = { this.state.bio }
-            onChange = { this.handleChange }
-            fullWidth /
-            >
-            <
-            TextField name = "website"
-            tpye = "text"
-            label = "Website"
-            placeholder = "Your personal/professinal website"
-            className = { classes.textField }
-            value = { this.state.website }
-            onChange = { this.handleChange }
-            fullWidth /
-            >
-            <
-            TextField name = "location"
-            tpye = "text"
-            label = "Location"
-            placeholder = "Where you live"
-            className = { classes.textField }
-            value = { this.state.location }
-            onChange = { this.handleChange }
-            fullWidth /
-            >
-            <
-            /form> < /
-            DialogContent > <
-            DialogActions >
-            <
-            Button onClick = { this.handleClose }
-            color = "primary" >
-            Cancel <
-            /Button> <
-            Button onClick = { this.handleSubmit }
-            color = "primary" >
-            Save <
-            /Button> < /
-            DialogActions > <
-            /Dialog> < /
-            Fragment >
+        return (
+            <Fragment>
+                <MyButton
+                    tip="Edit Details"
+                    onClick={this.handleOpen}
+                    btnClassName={classes.button}>
+                    <EditIcon color="primary" />
+                </MyButton>{" "}
+                <Dialog
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                    fullWidth
+                    maxWidth="sm">
+                    <DialogTitle> Edit your details </DialogTitle>{" "}
+                    <DialogContent>
+                        <form>
+                            <TextField
+                                name="bio"
+                                tpye="text"
+                                label="Bio"
+                                multiline
+                                rows="3"
+                                placeholder="A short bio about yourself"
+                                className={classes.textField}
+                                value={this.state.bio}
+                                onChange={this.handleChange}
+                                fullWidth
+                            />
+                            <TextField
+                                name="website"
+                                tpye="text"
+                                label="Website"
+                                placeholder="Your personal/professinal website"
+                                className={classes.textField}
+                                value={this.state.website}
+                                onChange={this.handleChange}
+                                fullWidth
+                            />
+                            <TextField
+                                name="location"
+                                tpye="text"
+                                label="Location"
+                                placeholder="Where you live"
+                                className={classes.textField}
+                                value={this.state.location}
+                                onChange={this.handleChange}
+                                fullWidth
+                            />
+                        </form>{" "}
+                    </DialogContent>{" "}
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            Cancel{" "}
+                        </Button>{" "}
+                        <Button onClick={this.handleSubmit} color="primary">
+                            Save{" "}
+                        </Button>{" "}
+                    </DialogActions>{" "}
+                </Dialog>{" "}
+            </Fragment>
         );
     }
 }
@@ -142,6 +137,6 @@ const mapStateToProps = (state) => ({
     credentials: state.user.credentials
 });
 
-export default connect(
-    mapStateToProps, { editUserDetails }
-)(withStyles(styles)(EditDetails));
+export default connect(mapStateToProps, { editUserDetails })(
+    withStyles(styles)(EditDetails)
+);
